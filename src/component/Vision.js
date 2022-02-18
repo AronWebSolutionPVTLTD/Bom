df40d34a40ee17c4f0d5815f1c412ce775cad3db
aronwebsolution <44797871+VijayTahkur@users.noreply.github.com>
2022-02-18 13:40:43 +0530

    new updates

--- src/component/Vision.js
+++ src/component/Vision.js
@@ -10,8 +10,11 @@ export default function Vision(){
                         <Image imageName={vision} alt={vision}></Image>
                     </div>
                     <div class="cols-2">
-                        <h2 className="font-sansation text-40 leading-42 font-bold">BOM’s<br/> Grand Vision</h2>
-                        <p className='text-20 leading-30'>BOM’s world-class team dreamt of an ecosystem that propels businesses forward like nothing before, while highly rewarding investors and contributors throughout the process. We’ll allow countless businesses to benefit from blockchain technology that enables leveraging stocks as a payment method, rather than opting for third-party services and paying fees. Essentially, participants will be co-owners of the ecosystem.</p>
+                        <div className='max-w-530'>
+                            <h2 className="font-sansation text-40 leading-42 font-bold">BOM’s<br/> Grand Vision</h2>
+                            <p className='text-20 leading-30 my-7'>BOM’s world-class team dreamt of an ecosystem that propels businesses forward like nothing before, while highly rewarding investors and contributors throughout the process. We’ll allow countless businesses to benefit from blockchain technology that enables leveraging stocks as a payment method, rather than opting for third-party services and paying fees. Essentially, participants will be co-owners of the ecosystem.</p>
+                            <a href="" className='btn-primary'>Get Started!</a>
+                        </div>
                     </div>
                 </div>
             </div>
